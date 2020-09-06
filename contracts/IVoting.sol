7954c33b2bcb086db8dc0467c52c4052ff735e61
dapp-whisperer <69157701+dapp-whisperer@users.noreply.github.com>
2020-09-05 22:59:07 -0500

    Update IVoting interface

--- contracts/IVoting.sol
+++ contracts/IVoting.sol
@@ -1,6 +1,17 @@
 pragma solidity 0.6.8;
 
 interface IVoting {
+        /**
+    * @notice Create a new vote about "`_metadata`"
+    * @param _executionScript EVM script to be executed on approval
+    * @param _metadata Vote metadata
+    * @param _castVote Whether to also cast newly created vote
+    * @param _executesIfDecided Whether to also immediately execute newly created vote if decided
+    * @return voteId id for newly created vote
+    */
+    function newVote(bytes calldata _executionScript, string calldata _metadata, bool _castVote, bool _executesIfDecided)
+        external returns (uint256 voteId);
+
     /**
     * @notice Vote `_supports ? 'yes' : 'no'` in vote #`_voteId`
     * @dev Initialization check is implicitly provided by `voteExists()` as new votes can only be
